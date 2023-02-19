f1428f8a61f36d1e3c9890a32e355f4dd29c2761
Protick Roy <protickrr@gmail.com>
2023-02-19 19:47:44 +0600

    fs module

--- server-and-core-modules.js
+++ server-and-core-modules.js
@@ -13,9 +13,34 @@
 // console.log(baseName, directoryName, extName, parseRes);
 
 // os module
-const os = require("os");
-osPlatform = os.platform();
-osHomedir = os.homedir();
-osFreemem = os.freemem();
-cpuCoreCount = os.cpus();
-console.log(osPlatform, osHomedir, osFreemem, cpuCoreCount);
+// const os = require("os");
+// osPlatform = os.platform();
+// osHomedir = os.homedir();
+// osFreemem = os.freemem();
+// cpuCoreCount = os.cpus();
+// console.log(osPlatform, osHomedir, osFreemem, cpuCoreCount);
+
+// file system module 
+const fs = require("fs");
+
+// write something to file
+fs.writeFileSync("myFile.txt", "Hello world!");
+fs.writeFileSync("myFile.txt", "let replace previous content");
+
+// append content to file
+fs.appendFileSync("myFile.txt", "\nNew line of text appended\n");
+
+// read file 
+// stream and buffer is a seperate data type that is in binary format
+const data = fs.readFileSync("myFile.txt"); // synchronous/blocking program execution until file contents
+// are read from the file system
+console.log(data.toString());
+
+// asynchronous way of reading file contents 
+fs.readFile("myFile.txt", (err, data)=>{
+    if(err)
+        throw new Error(`Could not read from File ${err}`);
+    console.log(data.toString());
+});
+// asynchronous execution is always preferred
+
