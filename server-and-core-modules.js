e0e0f32b6c2e4f294bf2f5a58aa5bf792bbb1336
Protick Roy <protickrr@gmail.com>
2023-02-19 20:25:50 +0600

    events module: listener and emitter

--- server-and-core-modules.js
+++ server-and-core-modules.js
@@ -3,6 +3,8 @@
 // core/builtin modules to access system resources where nodejs runtime is running 
 // all modules provide us with some methods and properties
 
+
+
 // path module
 // const path = require("path");
 // const myPath = 'C:/Users/No/Desktop/projects/nodejs-practice/index.js';
@@ -12,6 +14,8 @@
 // const parseRes = path.parse(myPath);
 // console.log(baseName, directoryName, extName, parseRes);
 
+
+
 // os module
 // const os = require("os");
 // osPlatform = os.platform();
@@ -20,27 +24,45 @@
 // cpuCoreCount = os.cpus();
 // console.log(osPlatform, osHomedir, osFreemem, cpuCoreCount);
 
+
+
 // file system module 
-const fs = require("fs");
+// const fs = require("fs");
 
-// write something to file
-fs.writeFileSync("myFile.txt", "Hello world!");
-fs.writeFileSync("myFile.txt", "let replace previous content");
+// // write something to file
+// fs.writeFileSync("myFile.txt", "Hello world!");
+// fs.writeFileSync("myFile.txt", "let replace previous content");
 
-// append content to file
-fs.appendFileSync("myFile.txt", "\nNew line of text appended\n");
+// // append content to file
+// fs.appendFileSync("myFile.txt", "\nNew line of text appended\n");
 
-// read file 
-// stream and buffer is a seperate data type that is in binary format
-const data = fs.readFileSync("myFile.txt"); // synchronous/blocking program execution until file contents
-// are read from the file system
-console.log(data.toString());
+// // read file 
+// // stream and buffer is a seperate data type that is in binary format
+// const data = fs.readFileSync("myFile.txt"); // synchronous/blocking program execution until file contents
+// // are read from the file system
+// console.log(data.toString());
 
-// asynchronous way of reading file contents 
-fs.readFile("myFile.txt", (err, data)=>{
-    if(err)
-        throw new Error(`Could not read from File ${err}`);
-    console.log(data.toString());
+// // asynchronous way of reading file contents 
+// fs.readFile("myFile.txt", (err, data)=>{
+//     if(err)
+//         throw new Error(`Could not read from File ${err}`);
+//     console.log(data.toString());
+// });
+// // asynchronous execution is always preferred
+
+
+
+// events module 
+// nodejs is a non-blocking event driven runtime
+const EventEmitter = require("events"); // returns EventEmitter class
+const emitter = new EventEmitter();
+
+// register an event listener for "bellRing" event
+emitter.on("bellRing", function(){
+    console.log("Event er barota baijja geche !");
 });
-// asynchronous execution is always preferred
+
+// emitting event
+emitter.emit("bellRing");
+// listener must be registered prior to emitting event
 
