fc308e965f9f7e878f7cf19de7641f58c16e64fa
Protick Roy <protickrr@gmail.com>
2023-02-20 18:09:56 +0600

    http module: request data is streamed

--- stream-and-buffer.js
+++ stream-and-buffer.js
@@ -7,11 +7,54 @@
 // operate on that buffer unless a buffer is complete
 
 // read stream: 
-const fs = require("fs");
-const bigDataReadStream = fs.createReadStream(`${__dirname}/bigData.txt`, 'utf-8');
+// const fs = require("fs");
+// const bigDataReadStream = fs.createReadStream(`${__dirname}/bigData.txt`, 'utf-8');
 
-bigDataReadStream.on("data", (chunk)=>{
-    console.log(chunk);
+// bigDataReadStream.on("data", (chunk)=>{
+//     console.log(chunk);
+// });
+
+// console.log("hello");
+
+
+
+// http server request body is streamed by default
+const http = require("http");
+const server = http.createServer((req, res)=>{
+    const requestBody = [];
+
+    if(req.url == "/"){
+        res.write(`
+            <html>
+                <head>
+                    <title> Request body is streamed to nodejs server </title>
+                </head>
+                <body>
+                    <form method="POST" action="/process">
+                        <input type="text" name="message"/>
+                    </form>
+                </body>
+            </html>
+        `);
+
+    }else if(req.url == "/process"){
+        res.write("processing !");
+
+        req.on("data", (chunk)=>{
+            requestBody.push(chunk);
+        });
+
+        req.on("end", ()=>{
+            console.log("stream finished");
+            wholeReq = Buffer.concat(requestBody);
+            console.log(wholeReq.toString());
+        });
+
+    }else{
+        res.write("path not found !");
+    }
+    res.end();
 });
 
-console.log("hello");
+server.listen(3000);
+console.log("listening on port 3000");
